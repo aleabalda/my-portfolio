aaf3468018049ccbec47f37ff8f3dafd27358cc2
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-05-23 15:17:22 -0600

    menu closes on nav

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
         <span className="menu-icon" onClick={toggleMenu}>
           <MenuIcon />
         </span>
-        {menuOpen && <Menu />}
+        {menuOpen && <Menu toggle={toggleMenu} />}
       </div>
     </header>
   );
--- portfolio/src/components/Menu.tsx
+++ portfolio/src/components/Menu.tsx
@@ -1,23 +1,27 @@
-export const Menu = () => {
+interface MenuProps {
+  toggle: () => void;
+}
+
+export const Menu = ({ toggle }: MenuProps) => {
   return (
     <ul className="menu">
       <li className="menu-item">
-        <a href="#projects-section" className="menu-link">
+        <a href="#projects-section" className="menu-link" onClick={toggle}>
           Projects
         </a>
       </li>
       <li className="menu-item">
-        <a href="#skills-section" className="menu-link">
+        <a href="#skills-section" className="menu-link" onClick={toggle}>
           Skills
         </a>
       </li>
       <li className="menu-item">
-        <a href="#about-section" className="menu-link">
+        <a href="#about-section" className="menu-link" onClick={toggle}>
           About
         </a>
       </li>
       <li className="menu-item">
-        <a href="#contact-section" className="menu-link">
+        <a href="#contact-section" className="menu-link" onClick={toggle}>
           Contact
         </a>
       </li>
